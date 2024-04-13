ad956cba0e92064857c95ea73819277fd5472f61
cmedina-dev <christian.medina.atx@gmail.com>
2024-04-12 19:15:46 -0700

    test: refactor test functions with common assertion

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,9 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("say hello when a name is provided", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("say 'Hello, World' when no name is provided", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
